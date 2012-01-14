b776301a2d814b7412996a764d84991d1bc6bd2b
koyachi <rtk2106@gmail.com>
2012-01-15 02:23:22 +0900

    remove debug code

--- spec/string-witchy_spec.rb
+++ spec/string-witchy_spec.rb
@@ -6,11 +6,9 @@ require 'string-witchy'
 describe String::Witchy do
   before do
     @string = 'total cost'
-    @string2 = 'String extension for witchy'
   end
 
   it 'should wichied' do
     @string.witchy.should eql('†O†ΔL COS†')
-    p @string2.witchy
   end
 end
